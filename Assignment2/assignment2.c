f55480c13580ab186f012194d3e26517f9c7b9dd
Denghaotian <578811809@qq.com>
2020-02-20 10:19:17 -0500

    the third version

--- Assignment2/assignment2.c
+++ Assignment2/assignment2.c
@@ -11,7 +11,7 @@ pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
 void* ThreadEntry(void* id)   //aceess threads
 {
     const int myid = (long)id;
-    printf("Thread:%ld\n",myid);
+    printf("ThreadEntry_num:%ld\n",myid);
     const int workloops = 5;
     int i;
     for ( i=0; i<workloops; i++)
@@ -19,34 +19,43 @@ void* ThreadEntry(void* id)   //aceess threads
         sleep(1);
        printf("sleep:%d\n",i);
     }
-
+    printf("mutex_state_entry_before_lock:%d\n",mutex);
     pthread_mutex_lock(&mutex); //lock mutex
 
-    done ++;
+    printf("mutex_state_entry_after_lock:%d\n",mutex);
+   
+     done ++;
     printf("done:%d\n",done);
 
     pthread_cond_signal(&cond);
+    printf("after_cond_signal,cond:%d mutex:%d\n",cond,mutex);
     pthread_mutex_unlock(&mutex);  //unlock mutex
 
+    printf("after_mutex_unlock,cond:%d mutex:%d\n",cond,mutex);
     return NULL;
 }
 
 int main (int argc, char** argv)
 {
+    printf("-----------start--------------------\n");
+    
     pthread_t threads[NUMTHREADS]; // declare threads ID
-
+    printf("number of threads:%d\n",NUMTHREADS);
+    
     int t;
     for(t=0; t<NUMTHREADS; t++)
     { 
-      printf("num_threads:%d\n",t); 
+      printf("create_num_threads:%d\n",t); 
       pthread_create( &threads[t],NULL,ThreadEntry,(void*)(long)t );
     }
     pthread_mutex_lock( &mutex );
+    printf("mutex_state_mainfunction:%d\n",mutex);     
 
     while( done < NUMTHREADS )
     {
-       printf("cond:%d mutex:%d\n",cond,mutex);
+       printf("state_before_cond_waitfunction cond:%d mutex:%d\n",cond,mutex);
        pthread_cond_wait (& cond, & mutex );
+       printf("state_after_cond_wait_function:cond:%d mutex:%d\n",cond,mutex);
     }
 
     pthread_mutex_unlock( &mutex );
